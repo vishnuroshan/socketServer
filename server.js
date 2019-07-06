78d358c2b4df37d51f2fcd3c2d387fa351a7ce6d
vishnuroshan <vishnuroshan4@gmail.com>
2019-07-06 18:04:26 +0530

    port

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const fs = require('fs');
 const routes = require('./routes/auth');
 const app = express();
 const ip = '172.16.20.95';
-const http_port = 3000;
+// const http_port = 3000;
+const port = process.env.PORT || 8000;
 var Welcome_html = fs.readFileSync('index.html');
 
 var http_server = http.createServer();
 http_server.on('request', app);
-http_server.listen(http_port, function() {
+http_server.listen(port, function() {
 	console.log(
 		'Listening on ' +
 			http_server.address().address +
