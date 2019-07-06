0fdd6c0b5ff28a6ab99c00ce86973df61878bf9a
vishnuroshan <vishnuroshan4@gmail.com>
2019-07-06 17:58:00 +0530

    working commit

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 'use strict';
 
-import express from 'express';
-import { createServer } from 'http';
-import { readFileSync } from 'fs';
-import routes from './routes/auth';
+const express = require('express');
+const http = require('http');
+const fs = require('fs');
+const routes = require('./routes/auth');
 const app = express();
 const ip = '172.16.20.95';
-const https_port = 3000;
-var Welcome_html = readFileSync('index.html');
+const http_port = 3000;
+var Welcome_html = fs.readFileSync('index.html');
 
-var http_server = createServer();
+var http_server = http.createServer();
 http_server.on('request', app);
-http_server.listen(https_port, function() {
+http_server.listen(http_port, function() {
 	console.log(
 		'Listening on ' +
 			http_server.address().address +
