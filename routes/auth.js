79d716bb0a6c52937ba01d5f9f4a76bbb8f98f6c
vishnuroshan <vishnuroshan4@gmail.com>
2019-07-06 18:10:42 +0530

    req.body conoled

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/chatbot', (req, res) => {
-	console.log(req);
+	console.log(req.body);
 	res.status(200).json({
 		status: 'SUCCESS'
 	});
